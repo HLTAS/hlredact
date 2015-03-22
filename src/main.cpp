30284180787bd39ce70970168384942e8b190648
Matherunner <randolphchong@gmail.com>
2015-03-23 00:47:28 +0800

    Set locale to C
    This fixes hltas not parsing numbers with dot as the decimal separator
    correctly under European locales.  Also fixes numbers with dot not accepted in
    input fields.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <QApplication>
+#include <QLocale>
+#include <locale>
 #include "mainwindow.hpp"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    std::locale::global(std::locale("C"));
+    QLocale::setDefault(QLocale::c());
     RMainWindow mainwin;
     mainwin.show();
     return app.exec();
